f3bff3114b860ee3ab97dcd0ae08c76c0b690cf9
0line <evolution.servicios@gmail.com>
2016-02-07 19:20:52 -0600

    hola

--- SIG/src/controller/Main.java
+++ SIG/src/controller/Main.java
@@ -1,18 +1,21 @@
 package controller;
 	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception{
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			String Patch="../views/fxml/prueba.fxml";
+			AnchorPane root = FXMLLoader.load(getClass().getResource(Patch));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("../views/css/main.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
